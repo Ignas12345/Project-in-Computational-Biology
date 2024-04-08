fdbc34373eb16cbc4dbabb27dd7bd79fa45bedc2
Ignas12345 <155150364+Ignas12345@users.noreply.github.com>
2024-04-08 14:13:07 +0300

    Update converters.py

--- converters.py
+++ converters.py
@@ -1,14 +1,14 @@
 import torch
-import torchvision.transforms as T
+from torchvision.transforms import v2
 
 def image_to_tensor(image):
   tensor = T.ToTensor()(image)
-  return tensor.unsqueeze(0)
+  return v2.unsqueeze(0)
 
 def tensor_to_image(tensor):
   tensor = tensor.squeeze()
-  return T.ToPILImage()(tensor)
+  return v2.ToPILImage()(tensor)
 
 def turn_to_grayscale(image):
-  return T.v2.Grayscale()(image)
+  return v2.Grayscale()(image)
   
