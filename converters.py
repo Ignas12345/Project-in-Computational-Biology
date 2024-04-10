d0ae05214c1923e56ee26daee3c2c57accae1215
Ignas12345 <155150364+Ignas12345@users.noreply.github.com>
2024-04-10 08:42:56 +0300

    Update converters.py

--- converters.py
+++ converters.py
@@ -11,14 +11,17 @@ def generate_noise(image, upscaling_factor, mean = 0, std_dev = 0.2):
   #noise = (noise-np.min(noise))/(np.max(noise)-np.min(noise))
   return Image.fromarray(noise)
 
+#this function turns an image to a tensor with values betwwen -1 and 1 (since edge net is trained for such tensors and I just followed their convention)
 def image_to_tensor(image):
   tensor = v2.PILToTensor()(image)
-  tensor = (tensor-torch.min(tensor))/(torch.max(tensor)-torch.min(tensor))
+  tensor = (2.0*tensor-(torch.min(tensor)+torch.max(tensor)))/(torch.max(tensor)-torch.min(tensor))
   #tensor = v2.Normalize([0.5], [0.5])(tensor)
   return tensor.unsqueeze(0)
 
+#this function makes the tensor to be first in the range between 0 and 1 and then converts it to an image
 def tensor_to_image(tensor):
   tensor = tensor.squeeze()
+  tensor = (tensor + 1.0) / 2.0
   return v2.ToPILImage()(tensor)
 
 def turn_to_grayscale(image):
